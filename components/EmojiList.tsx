3364b3f593cba4333525158dac629f0b33ccaedb
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2024-11-07 10:09:30 -0500

    Add files via upload

---
+++ components/EmojiList.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { StyleSheet, FlatList, Platform, Pressable } from 'react-native';
+import { Image, type ImageSource } from 'expo-image';
+
+type Props = {
+  onSelect: (image: ImageSource) => void;
+  onCloseModal: () => void;
+};
+
+export default function EmojiList({ onSelect, onCloseModal }: Props) {
+  const [emoji] = useState<ImageSource[]>([
+    require("../assets/images/bug.png"),
+    require("../assets/images/guitar.png"),
+    require("../assets/images/heart_eyes.png"),
+    require("../assets/images/new_moon_with_face.png"),
+    require("../assets/images/see_no_evil.png"),
+    require("../assets/images/sunglasses.png"),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}>
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
